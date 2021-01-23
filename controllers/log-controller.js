5bf555f62df65c14bbf589ddc273e3a707f74964
Bob Roudebush <bob@roudybob.net>
2021-01-23 17:48:08 -0500

    finished with server side

--- controllers/log-controller.js
+++ controllers/log-controller.js
@@ -1,10 +1,6 @@
-const log = require('../models/log');
-
 const router = require('express').Router();
 const Log = require('../db').import('../models/log');
 const validateSession = require('../middleware/validate-session');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
 router.get('/', validateSession, function (req, res) {
     Log.findAll({
